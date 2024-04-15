2a9806433994fb353c6d299d28a096f69474588b
d-mrtnvch <dmm@anadeainc.com>
2024-04-16 01:30:34 +0200

    feat: getIdGeneratorFunction

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -225,8 +225,12 @@ function partialUsingArguments(fn, ...args1) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let startInit = startFrom;
+  return () => {
+    startInit += 1;
+    return startInit - 1;
+  };
 }
 
 module.exports = {
